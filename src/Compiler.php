99b5001ddae144cec12745838ec2a7af523d44c4
xffecd <x_ffecd@hotmail.com>
2017-01-06 15:19:33 +0800

    修改composer支持的最小稳定性参数，增加了对@extends语法的改造

--- src/Compiler.php
+++ src/Compiler.php
@@ -5,7 +5,14 @@ use \Illuminate\View\Compilers\BladeCompiler;
 
 class Compiler extends BladeCompiler {
 
-    protected function compileExtends($express) {
-        parent::compileExtends($express);
+    protected function compileExtends($expression) {
+        $path = $expression;
+        $fis = $this->app->make('fis');
+
+        if (Str::startsWith($path, '(')) {
+            $path = substr($path, 1, -1);
+        }
+        $path = $fis->uri($path);
+        return parent::compileExtends($path);
     }
 }
